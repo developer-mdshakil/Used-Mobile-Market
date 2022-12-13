767f0ad5dbe4bc608ab75632921ad79bf66cb612
developer-mdshakil <developerdevelopershakil.cmt1819@gmail.com>
2022-12-13 22:07:08 +0600

    basic projects setup with router

---
+++ src/Routes/Routes/Routers.js
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Homepage/Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+        ]
+    }
+]);
+
+
+export default router;
